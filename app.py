ee01a769010e52f7eef918b46362ef29443c58c6
pradneshhhh <54749356+pradneshhhh@users.noreply.github.com>
2019-12-18 23:15:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from flask import Flask,render_template,url_for,request
 import joblib
 import pickle
 from nltk.corpus import stopwords 
-stopwords = set(stopwords.words('english'))
+#stopwords = set(stopwords.words('english'))
+stopwords = ['ourselves', 'hers', 'between', 'yourself', 'but', 'again', 'there', 'about', 'once', 'during', 'out', 'very', 'having', 'with', 'they', 'own', 'an', 'be', 'some', 'for', 'do', 'its', 'yours', 'such', 'into', 'of', 'most', 'itself', 'other', 'off', 'is', 's', 'am', 'or', 'who', 'as', 'from', 'him', 'each', 'the', 'themselves', 'until', 'below', 'are', 'we', 'these', 'your', 'his', 'through', 'don', 'nor', 'me', 'were', 'her', 'more', 'himself', 'this', 'down', 'should', 'our', 'their', 'while', 'above', 'both', 'up', 'to', 'ours', 'had', 'she', 'all', 'no', 'when', 'at', 'any', 'before', 'them', 'same', 'and', 'been', 'have', 'in', 'will', 'on', 'does', 'yourselves', 'then', 'that', 'because', 'what', 'over', 'why', 'so', 'can', 'did', 'not', 'now', 'under', 'he', 'you', 'herself', 'has', 'just', 'where', 'too', 'only', 'myself', 'which', 'those', 'i', 'after', 'few', 'whom', 't', 'being', 'if', 'theirs', 'my', 'against', 'a', 'by', 'doing', 'it', 'how', 'further', 'was', 'here', 'than']
 
 tfidf_vect_pkl = open('tfidf_vectorizer.pkl','rb')
 tfidf_vect = joblib.load(tfidf_vect_pkl)
@@ -38,7 +39,6 @@ def predict():
             store_item_str=store_item_str.replace("-"," ")
             store_item_str=store_item_str.replace("_","")
             sentance=' '.join(e.lower() for e in store_item_str.split() if e.lower() not in stopwords)
-            print(sentance)
             store_items_list=[sentance]
             test_data_vect=tfidf_vect.transform(store_items_list)
             clf_prediction = clf.predict(test_data_vect)
