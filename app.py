ecc63ca2eadd4e4a5e1cfcc951d6d3a5c08c2b20
pradneshhhh <54749356+pradneshhhh@users.noreply.github.com>
2019-12-22 21:55:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ import pickle
 #stopwords = set(stopwords.words('english'))
 stopwords = ['ourselves', 'hers', 'between', 'yourself', 'but', 'again', 'there', 'about', 'once', 'during', 'out', 'very', 'having', 'with', 'they', 'own', 'an', 'be', 'some', 'for', 'do', 'its', 'yours', 'such', 'into', 'of', 'most', 'itself', 'other', 'off', 'is', 's', 'am', 'or', 'who', 'as', 'from', 'him', 'each', 'the', 'themselves', 'until', 'below', 'are', 'we', 'these', 'your', 'his', 'through', 'don', 'nor', 'me', 'were', 'her', 'more', 'himself', 'this', 'down', 'should', 'our', 'their', 'while', 'above', 'both', 'up', 'to', 'ours', 'had', 'she', 'all', 'no', 'when', 'at', 'any', 'before', 'them', 'same', 'and', 'been', 'have', 'in', 'will', 'on', 'does', 'yourselves', 'then', 'that', 'because', 'what', 'over', 'why', 'so', 'can', 'did', 'not', 'now', 'under', 'he', 'you', 'herself', 'has', 'just', 'where', 'too', 'only', 'myself', 'which', 'those', 'i', 'after', 'few', 'whom', 't', 'being', 'if', 'theirs', 'my', 'against', 'a', 'by', 'doing', 'it', 'how', 'further', 'was', 'here', 'than']
 
-#tfidf_vect_pkl = open('tfidf_vectorizer.pkl','rb')
-#tfidf_vect = joblib.load(tfidf_vect_pkl)
-#clf_pkl = open('svm_model.pkl','rb')
-#clf = joblib.load(clf_pkl)
-tfidf_vect = pickle.load(open('tfidf_vectorizer.pkl','rb'))
-clf = pickle.load(open('svm_model.pkl','rb'))
+tfidf_vect_pkl = open('tfidf_vectorizer.pkl','rb')
+tfidf_vect = joblib.load(tfidf_vect_pkl)
+clf_pkl = open('svm_model.pkl','rb')
+clf = joblib.load(clf_pkl)
+#tfidf_vect = pickle.load(open('tfidf_vectorizer.pkl','rb'))
+#clf = pickle.load(open('svm_model.pkl','rb'))
 app = Flask(__name__)
 
 @app.route('/')
